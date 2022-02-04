51035513b408aa9c1871a0818cd975ab78e7a479
sandrewtx08 <sandrewtx08@protonmail.com>
2022-02-04 16:30:48 +0000

    middlewares e routes

--- routes/index.ts
+++ routes/index.ts
@@ -56,32 +56,30 @@ router.use("/", segundoNomeSpace);
 
 router.use("/remove", remove);
 
-router.use("/", creditoCaracteres);
-router.use("/", creditoEmpty);
-router.use("/", creditoSpace);
-router.use("/", creditoNumber);
-
-router.use("/registro", registro);
-
-router.use("/", destinatarioEmpty);
-router.use("/", destinatarioInvalido);
-router.use("/", destinatarioCpfCaracteres);
-router.use("/", destinatarioCpfEmpty);
-router.use("/", destinatariocpfMaiorLength);
-router.use("/", destinatarioCpfMenorLength);
-router.use("/", destinatarioCpfSpace);
-router.use("/", destinatarioPrimeiroNomeCaracteres);
-router.use("/", destinatarioPrimeiroNomeEmpty);
-router.use("/", destinatarioPrimeiroNomeSpace);
-router.use("/", destinatarioSegundoNomeCaracteres);
-router.use("/", destinatarioSegundoNomeEmpty);
-router.use("/", destinatarioSegundoNomeSpace);
-router.use("/", depositoEmpty);
-router.use("/", depositoNumber);
-router.use("/", depositoSpace);
-router.use("/", depositoCaracteres);
-router.use("/", depositoMaximo);
-
+router.use("/deposito", destinatarioEmpty);
+router.use("/deposito", destinatarioInvalido);
+router.use("/deposito", destinatarioCpfCaracteres);
+router.use("/deposito", destinatarioCpfEmpty);
+router.use("/deposito", destinatariocpfMaiorLength);
+router.use("/deposito", destinatarioCpfMenorLength);
+router.use("/deposito", destinatarioCpfSpace);
+router.use("/deposito", destinatarioPrimeiroNomeCaracteres);
+router.use("/deposito", destinatarioPrimeiroNomeEmpty);
+router.use("/deposito", destinatarioPrimeiroNomeSpace);
+router.use("/deposito", destinatarioSegundoNomeCaracteres);
+router.use("/deposito", destinatarioSegundoNomeEmpty);
+router.use("/deposito", destinatarioSegundoNomeSpace);
+router.use("/deposito", depositoEmpty);
+router.use("/deposito", depositoNumber);
+router.use("/deposito", depositoSpace);
+router.use("/deposito", depositoCaracteres);
+router.use("/deposito", depositoMaximo);
 router.use("/deposito", desposito);
+
+router.use("/registro", creditoCaracteres);
+router.use("/registro", creditoEmpty);
+router.use("/registro", creditoSpace);
+router.use("/registro", creditoNumber);
+router.use("/registro", registro);
 
 export default router;
